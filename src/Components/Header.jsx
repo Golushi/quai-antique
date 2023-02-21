f7bab3424c5d7c410eea0d8ad064cf2123447bc7
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-02-21 11:21:55 +0100

    Nav+startheader-modif1

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
   <div
     className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
     <div
-      className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      className="relative float-left -mr-[100%] w-full transition-transform duration-[100ms] ease-in-out motion-reduce:transition-none"
       data-te-carousel-item
       data-te-carousel-active>
       <img
@@ -21,7 +21,7 @@ export default function Header() {
         alt="Wild Landscape" />
     </div>
     <div
-      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[100ms] ease-in-out motion-reduce:transition-none"
       data-te-carousel-item>
       <img
         src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
@@ -29,7 +29,7 @@ export default function Header() {
         alt="Camera" />
     </div>
     <div
-      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[100ms] ease-in-out motion-reduce:transition-none"
       data-te-carousel-item>
       <img
         src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
