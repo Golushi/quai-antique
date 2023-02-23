5407dacefc19236f2fc5d15fa146d2821f11a463
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-02-23 20:00:02 +0100

    debut_horaires_2

--- src/Components/OpeningHours.jsx
+++ src/Components/OpeningHours.jsx
@@ -4,27 +4,28 @@ export default function OpeningHours() {
   return (
     <section id='horaires'>
       <div className='w-2/3 min-w-[260px] md:w-[700px] h-screen flex-container m-auto'>
-        <div className='border-2 border-yellow grid justify-items-stretch my-20'>
-          <h3 className='text-xl border-2 border-black text-myblack justify-self-center'>Horaires</h3>
+        <div className=' grid justify-items-stretch my-10'>
+          <h3 className='text-xl text-myblack justify-self-center'>Horaires</h3>
         </div>
-        <div className='flex-container'>
-          <div className='border-4 border-myred grid justify-items-start my-5'>
-            <p className='text-myyellow border border-black h-20'>Lundi</p>
-            <p className='text-myyellow border border-black h-20'>Mardi</p>
-            <p className='text-myyellow border border-black h-20'>Mercredi</p>
-            <p className='text-myyellow border border-black h-20'>Jeudi</p>
-            <p className='text-myyellow border border-black h-20'>Vendredi</p>
-            <p className='text-myyellow border border-black h-20'>Samedi</p>
-            <p className='text-myyellow border border-black h-20'>Dimanche</p>
+        <div className='grid grid-cols-3'>
+          <div className='grid justify-items-start my-2'>
+            <p className='text-myyellow h-20 items-center flex my-2'>Lundi</p>
+            <p className='text-myyellow h-20 items-center flex my-2'>Mardi</p>
+            <p className='text-myyellow h-20 items-center flex my-2'>Mercredi</p>
+            <p className='text-myyellow h-20 items-center flex my-2'>Jeudi</p>
+            <p className='text-myyellow h-20 items-center flex my-2'>Vendredi</p>
+            <p className='text-myyellow h-20 items-center flex my-2'>Samedi</p>
+            <p className='text-myyellow h-20 items-center flex my-2'>Dimanche</p>
           </div>
-          <div className='border-4 border-myred grid justify-items-end my-5'>
-            <p className='text-myyellow border border-black h-20'>Lundi</p>
-            <p className='text-myyellow border border-black h-20'>Mardi</p>
-            <p className='text-myyellow border border-black h-20'>Mercredi</p>
-            <p className='text-myyellow border border-black h-20'>Jeudi</p>
-            <p className='text-myyellow border border-black h-20'>Vendredi</p>
-            <p className='text-myyellow border border-black h-20'>Samedi</p>
-            <p className='text-myyellow border border-black h-20'>Dimanche</p>
+          <div></div>
+          <div className='grid justify-items-end my-5'>
+            <p className='text-myyellow h-20 my-2'>Midi 12:00-14:30<br/>Soir 19:00-23:00</p>
+            <p className='text-myyellow h-20 my-2'>Midi 12:00-14:30<br/>Soir 19:00-23:00</p>
+            <p className='text-myyellow h-20 my-2'>Midi 12:00-14:30<br/>Soir 19:00-23:00</p>
+            <p className='text-myyellow h-20 my-2'>Midi 12:00-14:30<br/>Soir 19:00-23:00</p>
+            <p className='text-myyellow h-20 my-2'>Midi 12:00-14:30<br/>Soir 19:00-00:00</p>
+            <p className='text-myyellow h-20 my-2'>Midi 12:00-15:00<br/>Soir 19:00-00:00</p>
+            <p className='text-myyellow h-20 my-2'>Midi 12:00-15:00<br/>Soir 19:00-00:00</p>
           </div>
         </div>
       </div>
