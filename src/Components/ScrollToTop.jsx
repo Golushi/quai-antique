d73e360419dc002731fcf3b777ce4af67b81d495
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-02-24 12:10:37 +0100

    fin-footer-scrolltotop

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import STT from "../Assets/ScrollToTop/Scroll_House.png";
+import "../App.css";
+import ScrollToTop from 'react-scroll-to-top';
 
-export default function ScrollToTop() {
+export default function Scroll() {
   return (
-    <div className='w-[50px] h-[50px]'>
-      <img src={STT} className="" alt="Name" />
+    <div>
+        <ScrollToTop className='flex justify-center items-center' smooth color='#6F8695' />
     </div>
   )
 }
