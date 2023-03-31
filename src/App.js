41ef514266346d8d1a213769cf03bb5d34f11538
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2023-03-31 11:39:29 +0200

    modif home path

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ export default function App() {
     <BrowserRouter id="App" className="App">
       <AuthContextProvider>
         <Routes>
-          <Route exact path="/quai-antique" element={<Home />} />
-          <Route exact path="/" element={<Carte />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/carte" element={<Carte />} />
           <Route exact path="/admin" element={<Admin />} />
           {/* <Route path="/carte/*" element={<Navigate to="/Carte" />} /> */}
           <Route path="*" element={<Home />} />
